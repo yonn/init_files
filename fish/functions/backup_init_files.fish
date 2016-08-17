d571f399205a3264f49c1fdc02c6f85f3d929ac3
Yogev Onn <yogevonn@gmail.com>
2016-08-17 20:05:03 +0300

    Updated files...

---
+++ fish/functions/backup_init_files.fish
@@ -0,0 +1,11 @@
+function backup_init_files
+	cp ~/.emacs ~/init_files
+	cp ~/.vimrc ~/init_files
+	cp -r ~/.config/fish ~/init_files
+	pushd (pwd)
+	cd ~/init_files
+	git add *
+	git commit -m "Updated files..."
+	git push origin master
+	popd
+end
